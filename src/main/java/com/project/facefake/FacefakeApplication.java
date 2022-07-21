b6080f0bc6662f507ac78e2f4d2aec6ab0a1a018
Zouhair CHAKIR <zchakir11@gmail.com>
2022-07-21 21:18:12 +0100

    project spring boot initialisation

---
+++ src/main/java/com/project/facefake/FacefakeApplication.java
@@ -0,0 +1,13 @@
+package com.project.facefake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacefakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacefakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/facefake/FacefakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.facefake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacefakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
